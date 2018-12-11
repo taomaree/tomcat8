5cf5ae117a491e3fce47a5afaedef42c9244d576
Thomas Lee <litao.git@gmail.com>
2018-12-11 18:25:38 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,13 @@
+sed -i -r -e 's/(FINE|INFO)/OFF/g' ${CATALINA_BASE}/conf/logging.properties
+sed -i -r -e 's/(1catalina.org.apache.juli.FileHandler.level|java.util.logging.ConsoleHandler.level).*/\1 = INFO/g' ${CATALINA_BASE}/conf/logging.properties
+echo "adjust log level " ${CATALINA_BASE}/conf/logging.properties
+echo "TOMCAT_URIENCODING " $TOMCAT_URIENCODING
+echo "############################################################"
+
+
+#URIEncoding="UTF-8" TOMCAT_URIENCODING='UTF-8'
+test $TOMCAT_URIENCODING = 'UTF-8' && sed -i -r -e 's/URIEncoding=\S+/URIEncoding="UTF-8"/g' ${CATALINA_BASE}/conf/server.xml
+test $TOMCAT_URIENCODING = 'ISO-8859-1' && sed -i -r -e 's/URIEncoding=\S+/URIEncoding="ISO-8859-1"/g' ${CATALINA_BASE}/conf/server.xml
 
 #export CATALINA_OPTS="$CATALINA_OPTS -server"
 
